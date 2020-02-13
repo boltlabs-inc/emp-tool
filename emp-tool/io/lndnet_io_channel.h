82a7e72ac30c37cc91d4ed047ad440fd39ea2a24
Gijs Van Laer <gijs.vanlaer@gmail.com>
2020-02-13 13:52:53 -0500

    lndnetio: basic setup

---
+++ emp-tool/io/lndnet_io_channel.h
@@ -0,0 +1,51 @@
+#ifndef LND_NETWORK_IO_CHANNEL
+#define LND_NETWORK_IO_CHANNEL
+
+#include "emp-tool/io/io_channel.h"
+#include "go_io_channel.h"
+
+using std::string;
+
+namespace emp {
+
+/** @addtogroup IO
+  @{
+ */
+class LndNetIO: public IOChannel<LndNetIO> {
+public:
+	bool is_server;
+    long long peer_ptr;
+	LndNetIO(long long peer_ptr, bool is_server, bool quiet = false) {
+	    this->is_server = is_server;
+	    this->peer_ptr = peer_ptr;
+	}
+	void sync() {
+		int tmp = 0;
+		if(is_server) {
+			send_data(&tmp, 1);
+			recv_data(&tmp, 1);
+		} else {
+			recv_data(&tmp, 1);
+			send_data(&tmp, 1);
+			flush();
+		}
+	}
+
+	~NetIO() {
+
+	}
+
+	void send_data(const void * data, int len) {
+		Send(data, len, peer_ptr);
+	}
+
+	void recv_data(void  * data, int len) {
+		recv_msg = Receive(peer_ptr);
+		*data = recv_msg.r0;
+		len = recv_msg.r1;
+	}
+};
+
+}
+
+#endif  //LND_NETWORK_IO_CHANNEL
