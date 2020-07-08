92f8445cd24ce6a4b06a30a4c937f359a7ae52e7
Gijs Van Laer <gijs.vanlaer@gmail.com>
2020-07-08 11:33:08 -0400

    EMP-2: intial commit for using lnd networking

---
+++ emp-tool/io/lndnet_io_channel.h
@@ -0,0 +1,64 @@
+#ifndef LND_NETWORK_IO_CHANNEL
+#define LND_NETWORK_IO_CHANNEL
+
+#include "emp-tool/io/io_channel.h"
+
+using std::string;
+
+struct Receive_return {
+    char* r0; /* msg */
+    int r1; /* length */
+    char* r2; /* errStr */
+};
+
+typedef Receive_return (*cb_receive)(void*);
+typedef char* (*cb_send)(void*, int, void*);
+
+namespace emp {
+
+/** @addtogroup IO
+  @{
+ */
+    class LndNetIO: public IOChannel<LndNetIO> {
+    public:
+        bool is_server;
+        void* peer_ptr;
+        cb_receive recv;
+        cb_send send;
+        LndNetIO(void* peer_ptr, cb_send send, cb_receive recv, bool is_server, bool quiet = false) {
+            this->is_server = is_server;
+            this->peer_ptr = peer_ptr;
+            this->recv = recv;
+            this->send = send;
+        }
+        void sync() {
+            int tmp = 0;
+            if(is_server) {
+                send_data(&tmp, 1);
+                recv_data(&tmp, 1);
+            } else {
+                recv_data(&tmp, 1);
+                send_data(&tmp, 1);
+            }
+        }
+
+        ~LndNetIO() {
+
+        }
+
+        void flush() { }
+
+        void send_data(const void * data, int len) {
+            (*send)((char *) data, len, peer_ptr);
+        }
+
+        void recv_data(void  * data, int len) {
+            Receive_return recv_msg = (*recv)(peer_ptr);
+            data = recv_msg.r0;
+            len = recv_msg.r1;
+        }
+    };
+
+}
+
+#endif  //LND_NETWORK_IO_CHANNEL
